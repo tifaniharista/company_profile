6ca37e3d5323cccabdaf755d8090ee1090397e62
mohamadsyahlefi <piwork884@gmail.com>
2024-12-22 22:43:09 +0700

    add notification

--- daftar.php
+++ daftar.php
@@ -21,7 +21,8 @@ mysqli_stmt_bind_param($stmt, 'b', $cv); // 'b' untuk blob
 mysqli_stmt_execute($stmt);
 
 if (mysqli_stmt_affected_rows($stmt) > 0) {
-    header("Location: index.html");
+    // Menampilkan notifikasi setelah pendaftaran berhasil
+    echo "<script>alert('Terima kasih... Data anda sudah diterima, tunggu konfirmasi melalui email yang kami kirimkan (maksimal 7 hari kerja)'); window.location.href='index.html';</script>";
 } else {
     echo "Pendaftaran Gagal : " . mysqli_error($conn);
 }
